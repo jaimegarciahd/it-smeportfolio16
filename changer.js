8aaaac446579091adcd39306b4dcbf213a90b515
jaime garcia <jgarcia3.sp@mysummitps.org>
2016-12-05 19:59:17 +0000

    Creaded Color Changing Checkpoint

---
+++ changer.js
@@ -0,0 +1,7 @@
+var Colour = document.getElementById("Colour")
+var click = document.getElementById("click")
+
+click.addEventListener("click", fucntion(){
+    document.body.style.backgroundColor = Colour.value;
+    
+})
